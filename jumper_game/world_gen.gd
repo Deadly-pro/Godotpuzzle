9ff01c8e19768b92dc2e867233f38565f69d0a3a
Deadly-pro <ruyketsu.prajwal@gmail.com>
2024-08-15 15:14:30 +0530

    Update world_gen.gd

--- jumper_game/world_gen.gd
+++ jumper_game/world_gen.gd
@@ -1,32 +1,35 @@
 extends Node2D
 
 @onready var tiles = get_parent().get_node("/root/scene/TileMap2")
-@export  var noise_height_texture : NoiseTexture2D
-var source_id = 0
-var noise : Noise
+@export var noise_height_texture: NoiseTexture2D
 var width = 6
 var last_level = 0
+
 func _ready():
-	noise = noise_height_texture.noise
 	for i in range(20):
 		generate(i)
-func generate(y=last_level):
+
+func generate(y = last_level):
+	var noise = noise_height_texture.noise
 	for x in range(width):
-		var noise_value: float = noise.get_noise_2d(x,y)
-		if noise_value > 0.0:
-			if noise_value> 0.166:
-				tiles.set_cell(2,Vector2(x-3,y),0,Vector2i(0,1))
-			elif noise_value > 3.33:
-				tiles.set_cell(2,Vector2i(x-3,y),7,Vector2i(2,1))
-			else:
-				tiles.set_cell(2,Vector2i(x-3,y),3,Vector2i(4,1))
+		var noise_value: float = noise.get_noise_2d(x, y)
+		var position = Vector2i(x - width / 2, y)
+
+		if noise_value > 0.333:
+			tiles.set_cell(2, position, 0, Vector2i(0, 1))
+		elif noise_value > 0.166:
+			tiles.set_cell(2, position, 7, Vector2i(2, 1))
+		elif noise_value > 0.0:
+			tiles.set_cell(2, position, 3, Vector2i(4, 1))
+		elif noise_value < -0.333:
+			tiles.set_cell(2, position, 4, Vector2i(6, 2))
+		elif noise_value < -0.166:
+			tiles.set_cell(1, position, 5, Vector2i(6, 1))
 		else:
-			if noise_value< -0.166:
-				tiles.set_cell(1,Vector2i(x-3,y),5,Vector2i(6,1))
-			elif noise_value < -3.33:
-				tiles.set_cell(2,Vector2i(x-3,y),4,Vector2i(6,2))
-			else:
-				tiles.set_cell(2,Vector2i(x-3,y),6,Vector2i(5,1))
-		last_level += 1
+			tiles.set_cell(2, position, 6, Vector2i(5, 1))
+
+	last_level += 1
+
 func _process(delta):
 	pass
+
